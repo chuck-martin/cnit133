2147a27f7b434fe3ea5e7bac01da68c0a135a66f
Chuck Martin <chuck_martin@me.com>
2019-01-31 18:30:24 -0800

    lab 2 files (new and updated)

---
+++ lab2/lab2.js
@@ -0,0 +1,10 @@
+/*
+Chuck Martin
+CNIT 133 Lab 2: Hello World!
+January 31, 2019
+*/
+
+// Sets the text for the element with ID myMessage
+window.onload = function() {
+  document.getElementById("myMessage").innerHTML = "Hello, San Francisco!";
+}
