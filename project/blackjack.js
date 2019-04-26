fcb932905caa355ff0b58743755581ee1cf4b543
Chuck Martin <chuck_martin@me.com>
2019-04-26 09:27:11 -0700

    added, fixed, more code

--- project/blackjack.js
+++ project/blackjack.js
@@ -13,6 +13,7 @@ var turn = "player";
 var player;
 var dealer;
 
+
 // Class for blackjack hands
 class BlackjackHand {
   constructor(
@@ -32,26 +33,26 @@ class BlackjackHand {
 // When a card is dealt, updates the total for that hands
 // There can be more than one total if a hand contains one or more aces
   updateHandTotal() {
-    var testCard;
-    // Test if there are any cards
-    if (cards.length > 0) {
-      // loop through the cards
-      for (var i=0; i < cards.length; i++) {
-        // Get the first character, which is the card rank
-        testCard += cards[i].charAt(0);
-        // number card
-        if (testCard == "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
-          this.handTotal += parseInt(testCard);
-          // face card
-        } else if (testCard == "T" || "J" || "Q" || "K") {
-          this.handTotal += 10;
-        } else {
-          // It's an ace
-          this.handTotal += 1;
-          this.aceInHand = true;
-        }
+    var testCard = "";
+    // loop through the cards
+    for (var i=0; i < this.hand.length; i++) {
+      // Get the first character, which is the card rank
+      testCard += this.hand[i].charAt(0);
+      alert(testCard);
+      // number card
+      if (testCard == "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+        this.handTotal += parseInt(testCard);
+        // face card
+      } else if (testCard == "T" || "J" || "Q" || "K") {
+        this.handTotal += 10;
+        alert(this.handTotal);
+      } else {
+        // It's an ace
+        this.handTotal += 1;
+        this.aceInHand = true;
       }
     }
+    // alert(this.hand, + ", " + this.handTotal);
   }
 
 
@@ -60,35 +61,41 @@ class BlackjackHand {
 }
 function newGame() {
   // create player & dealer instances 
+  gameDeck = new DeckOfCards;
   player = new BlackjackHand;
   dealer = new BlackjackHand;
   document.getElementById("dealer").innerHTML = "";
   document.getElementById("player").innerHTML = "";
-  drawnCards = 0;
-  usedCards = new Array(52);
+  // drawnCards = 0;
+  // usedCards = new Array(52);
 }
 
 
 
 // This deals a card to the appropriate spot
 function dealCard() {
-  var dealtCard = Math.floor(Math.random() * cards.length)
+  var dealtCard = gameDeck.getRandomUnusedCard();
+  // alert(dealtCard);
   // test if card is dealt already
-  if (usedCards[dealtCard] == "drawn") {
-    getRandomUnusedCard();
+  /*
+  while (gameDeck.usedCards[dealtCard] == "drawn") {
+    dealtCard = gameDeck.getRandomUnusedCard();
   }
-  else {
+*/
+  
     // add the card to a hand
     if (turn == "player") {
-      player.hand.push(cards[dealtCard]);
-      player.updateHandTotal();
+      player.hand.push(dealtCard);
+      // player.updateHandTotal();
+      // document.getElementById("playerheader").innerHTML = player.handTotal.toString();
       turn = "dealer";
     } else {
-      dealer.hand.push(cards[dealtCard]);
-      dealer.updateHandTotal();
+      dealer.hand.push(dealtCard);
+      // dealer.updateHandTotal();
+      // document.getElementById("dealerheader").innerHTML = dealer.handTotal.toString();
       turn = "player";
     }
-  }
+  
   displayCards();
 }
 
@@ -97,7 +104,7 @@ function displayCards() {
   // dealer cards
   var cardHTML = "";
   for (var i = 0; i < dealer.hand.length; i++) {
-    cardHTML += "<img src='" + cardPath + dealer.hand[i] + "'>";
+    cardHTML += "<img src='" + gameDeck.cardPath + dealer.hand[i] + "'>";
   }
   document.getElementById("dealer").style.width = (i * 100) + "px";
   document.getElementById("dealer").innerHTML = cardHTML;
@@ -105,7 +112,7 @@ function displayCards() {
   // player cards
   var cardHTML = "";
   for (var i = 0; i < player.hand.length; i++) {
-    cardHTML += "<img src='" + cardPath + player.hand[i] + "'>";
+    cardHTML += "<img src='" + gameDeck.cardPath + player.hand[i] + "'>";
   }
   document.getElementById("player").style.width = (i * 100) + "px";
   document.getElementById("player").innerHTML = cardHTML;
--- project/cards.js
+++ project/cards.js
@@ -1,5 +1,51 @@
-// The card arrays
+var drawnNumbers = "";
 
+// The deck of cards object
+
+class DeckOfCards {
+  constructor(
+    deck = new Array("AC.png", "2C.png", "3C.png", "4C.png", "5C.png", "6C.png",
+    "7C.png", "8C.png", "9C.png", "TC.png", "JC.png", "QC.png", "KC.png",
+    "AD.png", "2D.png", "3D.png", "4D.png", "5D.png", "6D.png",
+    "7D.png", "8D.png", "9D.png", "TD.png", "JD.png", "QD.png", "KD.png",
+    "AH.png", "2H.png", "3H.png", "4H.png", "5H.png", "6H.png",
+    "7H.png", "8H.png", "9H.png", "TH.png", "JH.png", "QH.png", "KH.png",
+    "AS.png", "2S.png", "3S.png", "4S.png", "5S.png", "6S.png",
+    "7S.png", "8S.png", "9S.png", "TS.png", "JS.png", "QS.png", "KS.png"),
+    usedCards = new Array(52),
+    drawnCards = 0,
+    cardPath = "images/cards/")
+    {
+      this.deck = deck;
+      this.usedCards = usedCards;
+      this.drawnCards = drawnCards;
+      this.cardPath = cardPath;
+    }
+
+  // Gets a random card from a deck of cards
+  // Sets a drawn card to "drawn" in a parallel array so it cannot be drawn again
+  getRandomUnusedCard() {
+    var getCard = Math.floor(Math.random() * this.deck.length);
+    // drawnNumbers += ", " + getCard.toString();
+    // alert(drawnNumbers);
+    // is the card still there?
+    while (this.deck[getCard] == "") {
+      // Nope, draw again
+       // this.getRandomUnusedCard();
+       var getCard = Math.floor(Math.random() * this.deck.length);
+    }
+    // else {
+      // card is still in the deck
+      var card = this.deck[getCard];
+      this.usedCards[getCard] = "";
+      // this.drawnCards ++;
+      return card;
+    // }
+  }
+
+}
+
+/*
 var cardPath = "images/cards/";
 var cards = ["AC.png", "2C.png", "3C.png", "4C.png", "5C.png", "6C.png",
 "7C.png", "8C.png", "9C.png", "TC.png", "JC.png", "QC.png", "KC.png",
@@ -12,7 +58,7 @@ var cards = ["AC.png", "2C.png", "3C.png", "4C.png", "5C.png", "6C.png",
 var usedCards = new Array(52);
 var drawnCards = 0;
 
-/*
+
 window.onload = clearCards;
 
 function clearCards() {
@@ -26,31 +72,29 @@ function clearCards() {
   dCards.length = 0;
   pCards.length = 0;
 }
-*/
+
 
 
 // Gets a random card from a deck of cards
 // Sets a drawn card to "drawn" in a parallel array so it cannot be drawn again
 function getRandomUnusedCard() {
   var getCard;
-  if (drawnCards == 52) {
+  if (this.drawnCards == 52) {
     alert("No more cards in deck.");
     return;
   }
-  getCard = Math.floor(Math.random() * cards.length)
-  if (usedCards[getCard] == "drawn") {
+  getCard = Math.floor(Math.random() * this.deck.length)
+  if (this.usedCards[getCard] == "drawn") {
     getRandomUnusedCard();
   }
   else {
-    usedCards[getCard] = "drawn";
-    drawnCards ++;
-    // document.getElementById("card").src = cardPath + cards[getCard];
-    // document.getElementById("cardsused").innerHTML = drawnCards;
-    return cards[getCard];
+    this.usedCards[getCard] = "drawn";
+    this.drawnCards ++;
+    return this.deck[getCard];
   }
 }
 
-/*
+
 function dealCard() {
   var getCard = Math.floor(Math.random() * cards.length)
   if (usedCards[getCard] == "drawn") {
